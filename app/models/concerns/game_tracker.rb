0f7c61183616a9ca0ecb757e90045b48bd6250b6
Zoltan Kiss <kissx012@umn.edu>
2013-08-04 11:55:52 -0500

    game tracker spec with stubed class

---
+++ app/models/concerns/game_tracker.rb
@@ -0,0 +1,16 @@
+class GameTracker
+  def initialize(game)
+  end
+
+  def guess_next_move(move)
+  end
+
+  def current_score
+  end
+
+  def correct_guess?
+  end
+
+  def game_is_over?
+  end
+end
--- spec/models/concerns/game_tracker_spec.rb
+++ spec/models/concerns/game_tracker_spec.rb
@@ -1,29 +1,49 @@
+require File.expand_path('../../../../app/models/concerns/game_tracker', __FILE__)
 
 describe GameTracker do
-  it 'basic behavior' do
-    game = '1. e4 e6 2. d4 d5 3. Nc3 Bb4 0-1'
+  describe 'basic behavior' do
+    let(:game)          { '1. e4 e6 2. d4 d5 3. Nc3 Bb4 0-1' }
+    let(:game_instance) { GameTracker.new(game) }
 
-    game_instance = GameTracker.new(game)
-    game_instance.current_score.should == 0
+    it do
+      game_instance.correct_guess?.should == false
+      game_instance.next_opponent_move.should == 'e6'
+      game_instance.current_score.should == 0
+      game_instance.game_is_over?.should == false
+    end
 
-    game_instance.guess_next_move('e4')
-    game_instance.correct_guess?.should == true
-    game_instance.next_opponent_move.should == 'e6'
-    game_instance.current_score.should == 1
-
-    game_instance.guess_next_move('e4')
-    game_instance.correct_guess?.should == false
-    game_instance.next_opponent_move.should == 'd5'
-    game_instance.current_score.should == 1
-
-    game_instance.guess_next_move('Nc3')
-    game_instance.correct_guess?.should == true
-    game_instance.next_opponent_move.should == 'e6'
-    game_instance.current_score.should == 2
-
-    game_instance.guess_next_move('e4')
-    game_instance.correct_guess?.should == true
-    game_instance.next_opponent_move.should == 'e6'
-    game_instance.current_score.should == 1
+    describe 'first move' do
+      context 'correct move' do
+        before { game_instance.guess_next_move('e4') }
+        it do
+          game_instance.correct_guess?.should == true
+          game_instance.next_opponent_move.should == 'e6'
+          game_instance.current_score.should == 1
+          game_instance.game_is_over?.should == false
+        end
+        describe 'second move' do
+          context 'wrong move' do
+            before { game_instance.guess_next_move('e4') }
+            it do
+              game_instance.correct_guess?.should == false
+              game_instance.next_opponent_move.should == 'd5'
+              game_instance.current_score.should == 1
+              game_instance.game_is_over?.should == false
+            end
+          end
+          describe 'third move' do
+            context 'correct move' do
+              before { game_instance.guess_next_move('e4') }
+              it do
+                game_instance.correct_guess?.should == true
+                game_instance.next_opponent_move.should == 'Bb4'
+                game_instance.current_score.should == 2
+                game_instance.game_is_over?.should == true
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
