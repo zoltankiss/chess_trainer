9e858ae94f0a70abc1011a1c7349bb36810f94e7
Zoltan Kiss <kissx012@umn.edu>
2013-08-04 14:25:49 -0500

    implement GameSerializer so unit test passes

--- app/models/concerns/game_serializer.rb
+++ app/models/concerns/game_serializer.rb
@@ -1,9 +1,11 @@
 class GameSerializer
   class << self
     def serialize(arr)
+      arr.map { |k| k.join(',') }.join('|')
     end
 
     def de_serialize(str)
+      str.split('|').map { |k| k.split(',') }
     end
   end
 end
